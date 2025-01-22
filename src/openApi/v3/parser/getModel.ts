dce8cacd9a63e34b255cb37a627a684a5a8e3791
Yoshikuni Nonaka <ynonaka@jtechs.com>
2025-01-22 17:01:10 +0900

    Modify code generation to consider `default: null` as nullable

--- src/openApi/v3/parser/getModel.ts
+++ src/openApi/v3/parser/getModel.ts
@@ -27,7 +27,7 @@ export const getModel = (
         deprecated: definition.deprecated === true,
         isDefinition,
         isReadOnly: definition.readOnly === true,
-        isNullable: definition.nullable === true,
+        isNullable: definition.nullable === true || definition.default === null,
         isRequired: false,
         format: definition.format,
         maximum: definition.maximum,
